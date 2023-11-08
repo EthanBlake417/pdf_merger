f780d5ee9b15c0094d4d5fb1352b3aa1ae6a0cff
Ethan Blake <ethanblake417@gmail.com>
2023-11-08 10:49:19 -0700

    Second Commit

--- pdf_merger.py
+++ pdf_merger.py
@@ -71,12 +71,93 @@
 #
 # # Example usage:
 # combine_pdf_jpg(r'C:\Users\ethan\Desktop\sherri_album', 'sherri_album_music.pdf')
+#
+#
+# import os
+# from PIL import Image
+# from PyPDF2 import PdfReader, PdfWriter
+#
+#
+# class ImagePDFConverter:
+#     def __init__(self, input_directory, output_file):
+#         self.input_directory = input_directory
+#         self.output_file = output_file
+#
+#     def convert(self):
+#         # Initialize a list to hold PIL images and a PdfFileWriter object
+#         images = []
+#         pdf_writer = PdfWriter()
+#
+#         for filename in os.listdir(self.input_directory):
+#             file_path = os.path.join(self.input_directory, filename)
+#
+#             # Check if the file is the output file to avoid reading and writing the same file
+#             if file_path == self.output_file:
+#                 continue
+#
+#             if filename.endswith('.png') or filename.endswith('.jpg'):
+#                 # Open the image and convert it to RGB
+#                 image = Image.open(file_path).convert('RGB')
+#                 images.append(image)
+#             elif filename.endswith('.pdf'):
+#                 # Read the existing PDF and add its pages
+#                 pdf_reader = PdfReader(file_path)
+#                 for page in range(len(pdf_reader.pages)):
+#                     pdf_writer.add_page(pdf_reader.pages[page])
+#
+#         # Save all the images as a PDF
+#         if images:
+#             images[0].save(
+#                 self.output_file,
+#                 save_all=True,
+#                 append_images=images[1:],
+#                 quality=100,
+#                 resolution=100.0
+#             )
+#
+#         # Append the output PDF to the existing PDFs collected
+#         if os.path.exists(self.output_file):
+#             with open(self.output_file, "rb") as f:
+#                 pdf_reader = PdfReader(f)
+#                 for page in range(pdf_reader.getNumPages()):
+#                     pdf_writer.addPage(pdf_reader.getPage(page))
+#
+#             # Write the combined PDF to a new file
+#             with open(self.output_file, "wb") as f:
+#                 pdf_writer.write(f)
+#
+#         # Print success messages
+#         num_images = len(images)
+#         num_pdfs = len(pdf_writer.pages)
+#         if num_images > 0:
+#             print(f'Successfully converted {num_images} image(s) to PDF.')
+#         if num_pdfs > 0:
+#             print(f'Successfully appended {num_pdfs} page(s) from existing PDF(s).')
+#         if num_images == 0 and num_pdfs == 0:
+#             print(f'No images or PDF files found in {self.input_directory}!')
+#
+#
+# if __name__ == '__main__':
+#     # Usage
+#     # Make sure to import the required modules at the beginning of your script.
+#     # Create an instance of the class and call the convert method.
+#     converter = ImagePDFConverter('files', 'output.pdf')
+#     converter.convert()
+#
+#     # After conversion code
+#     output_file_path = 'output.pdf'  # Ensure this matches your actual output path
+#
+#     if os.path.isfile(output_file_path):
+#         print(f"The output file is located at: {output_file_path}")
+#         print(f"File size: {os.path.getsize(output_file_path)} bytes")
+#     else:
+#         print(f"The output file was not found. Please check the directory and permissions.")
 
 
 import os
 from PIL import Image
-from PyPDF2 import PdfFileReader, PdfFileWriter
-
+from PyPDF2 import PdfReader, PdfWriter
+import tempfile
 
 class ImagePDFConverter:
     def __init__(self, input_directory, output_file):
@@ -86,60 +167,53 @@ class ImagePDFConverter:
     def convert(self):
         # Initialize a list to hold PIL images and a PdfFileWriter object
         images = []
-        pdf_writer = PdfFileWriter()
+        pdf_writer = PdfWriter()
 
+        # Process images and PDFs in the directory
         for filename in os.listdir(self.input_directory):
             file_path = os.path.join(self.input_directory, filename)
-
-            # Check if the file is the output file to avoid reading and writing the same file
             if file_path == self.output_file:
                 continue
 
-            if filename.endswith('.png') or filename.endswith('.jpg'):
-                # Open the image and convert it to RGB
+            if filename.lower().endswith(('.png', '.jpg')):
                 image = Image.open(file_path).convert('RGB')
                 images.append(image)
-            elif filename.endswith('.pdf'):
-                # Read the existing PDF and add its pages
-                pdf_reader = PdfFileReader(file_path)
-                for page in range(pdf_reader.getNumPages()):
-                    pdf_writer.addPage(pdf_reader.getPage(page))
+            elif filename.lower().endswith('.pdf'):
+                pdf_reader = PdfReader(file_path)
+                for page in pdf_reader.pages:
+                    pdf_writer.add_page(page)
 
-        # Save all the images as a PDF
+        # Save all images to a temporary PDF if there are any
         if images:
-            images[0].save(
-                self.output_file,
-                save_all=True,
-                append_images=images[1:],
-                quality=100,
-                resolution=100.0
-            )
+            with tempfile.NamedTemporaryFile(delete=False) as tmp:
+                images[0].save(
+                    tmp.name,
+                    save_all=True,
+                    append_images=images[1:],
+                    quality=100,
+                    resolution=100.0
+                )
+                temp_pdf_reader = PdfReader(tmp.name)
+                for page in temp_pdf_reader.pages:
+                    pdf_writer.add_page(page)
+                os.unlink(tmp.name)  # Delete the temp file after adding pages to the writer
 
-        # Append the output PDF to the existing PDFs collected
-        if os.path.exists(self.output_file):
-            with open(self.output_file, "rb") as f:
-                pdf_reader = PdfFileReader(f)
-                for page in range(pdf_reader.getNumPages()):
-                    pdf_writer.addPage(pdf_reader.getPage(page))
-
-            # Write the combined PDF to a new file
-            with open(self.output_file, "wb") as f:
-                pdf_writer.write(f)
+        # Write the final PDF to the output file
+        with open(self.output_file, "wb") as f:
+            pdf_writer.write(f)
 
         # Print success messages
         num_images = len(images)
-        num_pdfs = pdf_writer.getNumPages()
+        num_pdfs = len(pdf_writer.pages)
         if num_images > 0:
             print(f'Successfully converted {num_images} image(s) to PDF.')
         if num_pdfs > 0:
-            print(f'Successfully appended {num_pdfs} page(s) from existing PDF(s).')
+            print(f'Successfully combined {num_pdfs} page(s) into {self.output_file}.')
         if num_images == 0 and num_pdfs == 0:
             print(f'No images or PDF files found in {self.input_directory}!')
 
 
 if __name__ == '__main__':
     # Usage
-    # Make sure to import the required modules at the beginning of your script.
-    # Create an instance of the class and call the convert method.
     converter = ImagePDFConverter('files', 'output.pdf')
     converter.convert()
